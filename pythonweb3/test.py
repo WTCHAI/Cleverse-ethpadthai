f68301c1ee3831c0506c3ff99a63367dcdaf7eb7
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-07-06 11:20:05 +0700

    Chain connencted querry to see what happended onchain

---
+++ pythonweb3/test.py
@@ -0,0 +1,19 @@
+from web3 import Web3 as web3Lib , EthereumTesterProvider
+
+
+# w3 = Web3(EthereumTesterProvider())
+w3Eth = web3Lib(web3Lib.HTTPProvider("https://eth-pokt.nodies.app"))
+
+isConnected = w3Eth.is_connected()
+print(isConnected)
+
+W3Binance = web3Lib(web3Lib.HTTPProvider("https://bsc.drpc.org"))
+BinanceBlockNumber = W3Binance.eth.block_number
+
+print(BinanceBlockNumber)
+
+Sepolia = web3Lib(web3Lib.HTTPProvider('https://1rpc.io/sepolia'))
+SepoliaAddress = "0xc3c52B4977937065bb8806065D71b832c0c6c8b7"
+SepoliaBlockNumber = Sepolia.eth.get_balance(SepoliaAddress)
+print(SepoliaBlockNumber)
+
